0322df2b1dd20dc1141d74796ce46418211b9eb4
martins0909 <158082763+martins0909@users.noreply.github.com>
2025-11-22 10:43:14 +0100

    feat(shop): reduce Confirm Purchase image size again for better above-the-fold fit

--- client/src/pages/Shop.tsx
+++ client/src/pages/Shop.tsx
@@ -984,7 +984,7 @@ const Shop = () => {
                   <img
                     src={selectedProduct.image}
                     alt={selectedProduct.name}
-                    className="w-24 h-24 md:w-36 md:h-36 object-cover rounded-xl"
+                    className="w-20 h-20 md:w-28 md:h-28 object-cover rounded-xl"
                   />
                 </div>
               </div>
